b82889b6a40b1d04410d4d2ccb1021676a546982
ValeryKharkov <kharkov.valery@gmail.com>
2022-05-26 20:29:36 +0300

    init gitignore

--- docstring.py
+++ docstring.py
@@ -6,13 +6,15 @@ from config import ARGUM1
 
 print(ARGUM1 + 10)
 
+
 def test(arg1, arg2):
     '''
-
     :param arg1: аргумент №1
     :param arg2: аргумент №2
     :return: результат
     '''
     pass
 
+
+print(test.__name__)
 print(test.__doc__)
--- main.py
+++ main.py
@@ -10,8 +10,6 @@ for i in range(250):
     phone = fake_ru.phone_number()
     job = fake_ru.job()
     address = fake_ru.address()
-    dict_[i] = {name:[address, job, phone]}
+    dict_[i] = {name: [address, job, phone]}
     with open('DS.txt', 'w', encoding='utf8') as f:
         json.dump(dict_, f, indent=4, ensure_ascii=False)
-
-
